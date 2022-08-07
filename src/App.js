6824976d87f2fd73b4914b4c841a0fdd7aea239c
akoval29 <akoval29@gmail.com>
2022-08-08 02:16:11 +0300

    Yup ref

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './app.scss';
 import FormikForm from './FormFormik';
+import YupForm from './FormYup';
 
 function App() {
   return (
     <div className="app">
-        <FormikForm/>
+        {/* <FormikForm/> */}
+        <YupForm/>
     </div>
   );
 }
