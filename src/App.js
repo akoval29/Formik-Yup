9012a63116aa0a9d4952d4628bc9daadd116cbc9
akoval29 <akoval29@gmail.com>
2022-08-08 04:25:54 +0300

    Props form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './app.scss';
 import FormikForm from './FormFormik';
 import YupForm from './FormYup';
+import PropsForm from './FormProps';
 
 function App() {
   return (
     <div className="app">
-        {/* <FormikForm/> */}
-        <YupForm/>
+        {/* <FormikForm/>
+        <YupForm/> */}
+        <PropsForm/>
     </div>
   );
 }
